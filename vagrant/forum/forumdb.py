9768790a5a4b9329d3d4e7ef885bd786f11449fb
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2019-03-02 20:14:22 +0200

    changing to sqlite

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,25 +2,22 @@
 
 import datetime
 import psycopg2
+import sqlite3
+
+db = sqlite3.connect('forum.sqlite')
+cur = db.cursor()
+
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database='forum')
-  c = db.cursor()
-  c.execute("select * from posts")
-  POSTS = c.fetchall()
-  c.close()
-  db.close()
-  return reversed(POSTS)
+  cur.execute("select time, content from posts")
+  return cur.fetchall()
+  
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  db = psycopg2.connect(database='forum')
-  c = db.cursor()
-  c.execute("insert into posts (content) values ('%s')" % content)
+  cur.execute("insert into posts (content) values (?)", (content,))
   db.commit()
-  db.close()
-
 
